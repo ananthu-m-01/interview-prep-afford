dcc3c2f9525078e5d64f11d101aa7a3be77cbeaa
Ananthu M <ananthu.m.utr@gmail.com>
2025-05-10 12:08:30 +0530

    debounce update

---
+++ src/assets/api/searchItems.js
@@ -0,0 +1,14 @@
+import dummyData from "../../data/dummyData";
+
+const searchItems = (query) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = dummyData.filter(item =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+      resolve(result);
+    }, 300);
+  });
+};
+
+export default searchItems;
